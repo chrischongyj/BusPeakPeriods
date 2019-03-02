ca8f40c54edc8a5b0d538a1910a388bdb92f3bdb
chrischongyj <chrischongyj@gmail.com>
2019-03-02 22:43:25 +0800

    Updated mainproject.py with querying of bus service and its load

--- mainproject.py
+++ mainproject.py
@@ -1,11 +1,24 @@
 import requests
+import json
 
-headers = { 'AccountKey': 'FP9dZIa6Tm6EsGeQWGRGOQ==', 
-            'accept': 'application/json',
-            'BusStopCode': '43289'}
+header = { 'AccountKey': 'FP9dZIa6Tm6EsGeQWGRGOQ==', 
+            'accept': 'application/json'}
+
+param = {'BusStopCode': '43679'}
 
 target = 'http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2'
 
-request = requests.get(target, params = headers)
+request = requests.get(target, params = param, headers = header)
 
-print(request.text)
+x = json.loads(request.text)
+
+print(json.dumps(x, indent = 2))
+
+for buses in x['Services']:
+    print("Service Number " + buses['ServiceNo'],
+    "Next Bus 1 = " + buses['NextBus']['Load'],
+    "Next Bus 2 = " + buses['NextBus2']['Load'],
+    "Next Bus 3 = " + buses['NextBus3']['Load'])
+
+
+
